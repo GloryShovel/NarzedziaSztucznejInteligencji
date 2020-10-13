199c1947d05b8d02a3903ab58acc6ecff35a9104
GloryShovel <sk.kamil6@wp.pl>
2020-10-13 15:40:16 +0200

    Lab 1 commit

---
+++ prefab.cpp
@@ -0,0 +1,9 @@
+#include <cv.hpp>
+#include <highgui.h>
+#include <iostream>
+int main( int argc, char** argv )
+{
+ cv::namedWindow("Okienko Prawie Inteligentne", 1);
+ cv::waitKey(0);
+ return 0;
+}
